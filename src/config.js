b1933ed09f098e1af492c2b994f3e5f808e0ff2d
Rishabhprocoder <137711326+Rishabhprocoder@users.noreply.github.com>
2024-07-05 12:01:20 +0530

    Set Up Configuration config.js

---
+++ src/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  ecomAPIs: [
+    'https://api.ecommerce1.com',
+    'https://api.ecommerce2.com',
+    'https://api.ecommerce3.com',
+    'https://api.ecommerce4.com',
+    'https://api.ecommerce5.com'
+  ]
+};
